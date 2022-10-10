9bca8e85617619130eecea6db4114895b24eb901
Adriano Raiano <adriano@raiano.ch>
2022-10-10 09:09:00 +0200

    lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import { babel } from '@rollup/plugin-babel';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { babel } from '@rollup/plugin-babel'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs'
 import pkg from './package.json'
 
 const getBabelOptions = ({ useESModules, plugins = [] }) => ({
