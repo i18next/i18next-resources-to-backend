b212ea38849fd90158241c4b63865c95cd577dd9
Adriano Raiano <adriano@raiano.ch>
2022-12-10 16:36:13 +0100

    update deps and extend typescript test

--- test/types/i18next-resources-to-backend.test-d.ts
+++ test/types/i18next-resources-to-backend.test-d.ts
@@ -1,7 +1,19 @@
-import { BackendModule } from 'i18next'
+import i18next, { BackendModule } from 'i18next'
 import resourcesToBackend from '../../'
 import { expectType } from 'tsd'
+
+import ChainedBackend, { ChainedBackendOptions } from 'i18next-chained-backend'
 
 expectType<BackendModule>(resourcesToBackend({ en: { translations: { key: 'value' } } }))
 expectType<BackendModule>(resourcesToBackend((lng: string, ns: string, clb) => ({ key: 'value' })))
 expectType<BackendModule>(resourcesToBackend(async (lng: string, ns: string) => ({ key: 'value' })))
+
+i18next.use(ChainedBackend).init<ChainedBackendOptions>({
+  backend: {
+    backends: [
+      resourcesToBackend({ en: { translations: { key: 'value' } } }),
+      resourcesToBackend((lng: string, ns: string, clb) => ({ key: 'value' })),
+      resourcesToBackend(async (lng: string, ns: string) => ({ key: 'value' }))
+    ]
+  }
+})
