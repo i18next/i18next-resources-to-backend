ab9e5340a9ff3289e24dc9976f7beddf5485b877
Adriano Raiano <adriano@raiano.ch>
2023-02-24 12:48:42 +0100

    simple example

---
+++ examples/simple/index.js
@@ -0,0 +1,25 @@
+import i18next from 'i18next'
+// import resourcesToBackend from 'i18next-resources-to-backend'
+import resourcesToBackend from '../../dist/esm/index.js'
+
+i18next
+  .use(resourcesToBackend((language, namespace) => import(`./locales/${language}/${namespace}.json`, { assert: { type: 'json' } })))
+
+await i18next.init({
+  // debug: true,
+  fallbackLng: {
+    default: ['en'],
+    en: ['en-US']
+  }
+})
+
+console.log('en: ' + i18next.t('hi'))
+
+await i18next.changeLanguage('en-US')
+console.log('en-US: ' + i18next.t('hi'))
+
+await i18next.changeLanguage('de')
+console.log('de: ' + i18next.t('hi'))
+
+await i18next.changeLanguage('de-CH')
+console.log('de-CH: ' + i18next.t('hi'))
