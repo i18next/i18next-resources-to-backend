011dd4aac754e49893a74ff8fc35845393fd451b
Jacques Yakoub <jacques.yakoub@gmail.com>
2024-04-06 23:36:13 +0200

    style: improve typings
    https://github.com/i18next/i18next-resources-to-backend/issues/11

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import { BackendModule, Resource, ReadCallback, ResourceKey } from 'i18next'
 
-type ImportFn = ((language: string, namespace: string, callback: ReadCallback) => void) | ((language: string, namespace: string) => Promise<ResourceKey | boolean | null | undefined>)
+type ImportFn<L, N> = ((language: L, namespace: N, callback: ReadCallback) => void) | ((language: L, namespace: N) => Promise<ResourceKey | boolean | null | undefined>)
 
-declare function resourcesToBackend(res: Resource | ImportFn): BackendModule
+declare function resourcesToBackend<L = string, N = string>(res: Resource | ImportFn<L, N>): BackendModule
 
 export default resourcesToBackend
